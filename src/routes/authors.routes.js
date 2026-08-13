eb36fc1807dce5a69453ce35d915f50accf0c9e4
Mariano Lovisotto <mariano.lovisotto@gmail.com>
2026-08-13 02:27:50 -0300

    feat: implematación de endpoints PUT y DELETE de authors y posts

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -51,5 +51,43 @@ router.post('/', async( res, req, next) => {
     }
 });
 
+router.put('/:id', async (req, res, next) =>{
+    try{
+        const {name, email, bio} = req.body;
+
+        if(!name || !email) {
+            throw new AppError('Name e email son obligatorios', 400);
+        }
+
+        const updated = await authorsService.update(
+            req.params.id,
+            { name, email, bio }
+        );
+
+        if(!updated) {
+            throw new AppError('Author no encontrado', 404);
+        }
+        res.json(updated);
+    }catch(error) {
+        if(error.code === '23505') {
+            return next(new AppError('El email ya existe', 400));
+        }
+        next(error);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deleted = await authorsService.deleteAuthor(req.params.id);
+
+        if(!deleted) {
+            throw new AppError('Author no encontrado', 404);
+        }
+        res.status(204).send();
+    }catch(error) {
+        next(error);
+    }
+});
+
 
 module.exports = router
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,44 +2,117 @@ const express = require('express');
 const router = express.Router();
 
 const postsService = require('../services/posts.service');
+const AppError = require('../middleware/appError');
 
 router.get('/', async (req, res) => {
-
-    const posts = await postsService.getAllPosts();
-    res.json(posts);
-});
-
-router.get('/:id', async (req, res) => {
-    const post = await postsService.getPostById(req.params.id);
-
-    if(!post) {
-        return res.status(404).json({ message: 'Post no encontrado'});
-    };
-    res.json(post);
-});
-
-router.get('/author/:authorId', async (req, res) => {
-    const posts = await postsService.getPostsByAuthor(req.params.authorId);
-    res.json(posts);
-});
-
-router.post('/', async (req, res) => {
-    const { author_id, title, content, published } = req.body;
-
-    if(!author_id || !title || !content) {
-        return res.status(400).json({
-            message: 'author_id, title y content son obligatorios',
-        });
+    try {
+        const posts = await postsService.getAllPosts();
+        res.json(posts);
+    }catch(error) {
+        next(error);
     }
-
-    const newPost = await postsService.createPost({
-        author_id,
-        title,
-        content,
-        published,
-    });
-
-    res.status(201).json(newPost);
 });
+
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const post = await postsService.getPostById(req.params.id);
+
+        if(!post) {
+            throw new AppError('Post no encontrado', 404)
+        }
+        res.json(post);
+    }catch(error) {
+        next(error);
+    }
+});
+
+
+router.get('/author/:authorId', async (req, res, next) => {
+    try{
+        const posts = await postsService.getPostByAuthor(req.params.authorId);
+        res.json(posts);
+    }catch(error) {
+        next(error);
+    }
+});
+
+
+router.post('/', async (req, res, next) =>{
+    try {
+        const { author_id, title, content, published} = req.body;
+
+        if(!author_id || !title || !content) {
+            throw new AppError(
+                'author_id, title y content son obligatorios',
+                400
+            );
+        }
+
+        const newPost = await postsService.createPost({
+            author_id,
+            title,
+            content,
+            published,
+        });
+        res.status(201).json(newPost);
+    }catch(error) {
+        if(error.code === '23503') {
+            return next(new AppError('El author_id no existe', 400));
+        }
+        next(error);
+    }
+})
+
+
+router.put('/:id', async (req, res, next) => {
+    try{
+        const data = req.body;
+
+        if(Object.keys(data).length === 0) {
+            throw new AppError('Debe enviar al menos un campo', 400);
+        }
+
+        if('title' in data && !data.title) {
+            throw new AppError('title no puede estar vacío', 400);
+        }
+
+        if('content' in data && !data.content) {
+            throw new AppError('content no puede estar vacío', 400);
+        }
+
+        if('author_id' in data && !data.author_id) {
+            throw new AppError('author_id no puede estar vacío')
+        }
+
+        const updated = await postsService.updatePost(
+            req.params.id,
+            data,
+        );
+
+        if(!updated) {
+            throw new AppError('Post no encontrado', 404);
+        }
+        res.json(updated);
+    }catch(error) {
+        if(error.code === '25503'){
+            return next(new AppError('El author_id no existe', 400));
+        }
+        next(error);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const deleted = await postsService.deletePost(req.params.id);
+
+        if(!deleted) {
+            throw new AppError('Post no encontrado', 404);
+        }
+        res.status(204).send();
+    }catch(error) {
+        next(error);
+    }
+})
 
 module.exports = router;
